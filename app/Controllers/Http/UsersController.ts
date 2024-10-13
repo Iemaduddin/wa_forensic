6b559137e72f7cd9ed3768a88b2d073811573c76
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2024-10-13 18:00:08 +0700

    fix user management and auth after merge conflict

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
-
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
 export default class UsersController {
-  public async index({ request, view }: HttpContextContract) {
-
+  public async index({ view }: HttpContextContract) {
     // const users = await Database.from('users').select('*').paginate(page, limit)
     const users = await Database.from('users').select('*')
     // Changes the baseURL for the pagination links
@@ -17,12 +16,10 @@ export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     try {
       const { email, username, password } = request.body()
-
       await User.create({ email, username, password })
 
       return response.redirect('back')
     } catch (error) {
- 
       return response.redirect('back')
     }
   }
@@ -34,30 +31,28 @@ export default class UsersController {
   public async update({ params, request, response }: HttpContextContract) {
     try {
       const { id } = params
-      const { email, username, password } = request.body()
+      const { username, password } = request.body()
+
+      // Definisikan schema validasi
+      const updateSchema = schema.create({
+        email: schema.string({}, [rules.email()]),
+      })
+
+      // Validasi input dari request
+      const payload = await request.validate({ schema: updateSchema })
 
       // Temukan user berdasarkan ID
       const user = await User.findOrFail(id)
-
       // Update user dengan data baru
-      user.email = email
+      user.email = payload.email
       user.username = username
       if (password) {
-        user.password = password // Pastikan password di-hash jika diperlukan
+        user.password = password
       }
       await user.save()
 
-      // const message = 'User updated successfully!'
-      // const success = true
-      // const users = await User.all()
-      // return view.render('users/main', { message, success, users })
-
       return response.redirect('back')
     } catch (error) {
-      // Tangani kesalahan jika terjadi
-      // const message = 'Something went wrong!'
-      // const success = false
-      // const users = await User.all()
       return response.redirect('back')
     }
   }
@@ -67,17 +62,8 @@ export default class UsersController {
       const { id } = params
       const user = await User.findOrFail(id)
       await user.delete()
-      // const message = 'User deleted successfully!'
-      // const success = true
-      // const users = await User.all()
-      // return view.render('users/main', { message, success, users })
-
       return response.redirect('back')
     } catch (error) {
-      // Tangani kesalahan jika terjadi
-      // const message = 'Something went wrong!'
-      // const success = false
-      // const users = await User.all()
       return response.redirect('back')
     }
   }
